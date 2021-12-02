6b4eab84d2eb366d7e65c20901e0ec705f561f05
islarobot <islarobots@gmail.com>
2021-12-02 23:54:09 +0100

    s

--- html/sonar_new_2.js
+++ html/sonar_new_2.js
@@ -1,151 +1,147 @@
-const displayElement = document.querySelector("#demo");
-const displayRect = displayElement.getBoundingClientRect();
-
-const width = displayRect.width;
-const height = displayRect.height;
-
-/**
- * Setup an off screen canvas context for drawing our gradient.
- * Note: don't the scale the context when generating a graphic for use as a pattern fill.
- */
-const offscreenElement = document.createElement("canvas");
-offscreenElement.width = width;
-offscreenElement.height = height;
-const offscreenCtx = offscreenElement.getContext("2d");
-
-const start = new Date();
-
-/**
- * Setup and draw the gradient.
- */
-const x0 = 0;
-const y0 = 0;
-const x1 = width;
-const y1 = height;
-const g = new SweepGradient.SweepGradient(offscreenCtx, x0, y0, x1, y1);
-
-g.addColorStop(0.0, "#ffff00");
-g.addColorStop(0.25, "#ff00ff");
-g.addColorStop(0.5, "red");
-g.addColorStop(0.75, "rgba(255,255,0,0)");
-g.addColorStop(1.0, "#ffff00");
-
-g.draw();
-
-var end = new Date();
-var elapsed = end.getTime() - start.getTime();
-console.log(`${elapsed} milliseconds`);
-
-/**
- * Get the gradient as a pattern.
- */
-const gradientPattern = offscreenCtx.createPattern(
-  offscreenCtx.canvas,
-  "no-repeat"
-);
-
-/**
- * Set up the canvas on the page to place our gradient
- */
-const dpr = Math.ceil(window.devicePixelRatio) || 1;
-displayElement.width = width * dpr;
-displayElement.height = height * dpr;
-const displayCtx = displayElement.getContext("2d");
-displayCtx.scale(dpr, dpr);
-
-/**
- * Draw a circle, which we fill using our gradient pattern.
- */
-const lineWidth = 40;
-displayCtx.strokeStyle = gradientPattern;
-displayCtx.lineWidth = lineWidth;
-displayCtx.arc(width / 2, height / 2, (width - lineWidth) / 2, 0, 2 * Math.PI);
-displayCtx.stroke();
 
 
+function pepe(vl,cd) {
 
-export class SweepGradient {
-  constructor(ctx, x0, y0, x1, y1) {
-    this.ctx = ctx;
-    this.x0 = Math.min(x0, x1);
-    this.y0 = Math.min(y0, y1);
-    this.x1 = Math.max(x0, x1);
-    this.y1 = Math.max(y0, y1);
+//console.log(vl.w_square)
 
-    this.width = this.x1 - this.x0;
-    this.height = this.y1 - this.y0;
+const vertices = [
+  -1.0, 1.0, 0.0,
+  -1.0, -1.0, 0.0,
+  1.0, -1.0, 0.0,
+  1.0, 1.0, 0.0
+];
+const indices = [0, 1, 2, 3];
+const height = vl.h_square;
+const width = vl.w_square;
 
-    this.gradientWidth = 2 * (this.width + this.height);
-    this.gradientHeight = 1;
+const canvas = document.createElement("canvas");
+canvas.height = height;
+canvas.width = width;
+const gl = canvas.getContext("webgl");
 
-    let offscreenCanvas = this.getOffscreenCanvas(
-      this.gradientWidth,
-      this.gradientHeight
-    );
-    this.offscreenCtx = offscreenCanvas.getContext("2d");
-    this.offscreenGradient = this.offscreenCtx.createLinearGradient(
-      0,
-      0,
-      this.gradientWidth,
-      this.gradientHeight
-    );
-  }
+// Create an empty buffer object to store vertex buffer
+const vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  addColorStop(offset, color) {
-    this.offscreenGradient.addColorStop(offset, color);
-  }
+// Create an empty buffer object to store Index buffer
+const Index_Buffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-  getOffscreenCanvas(w, h) {
-    if (window.OffscreenCanvas) {
-      return new window.OffscreenCanvas(w, h);
+// Vertex shader source code
+const vertCode = `
+    attribute vec3 coordinates;
+ 
+    void main(void) {
+      gl_Position = vec4(coordinates, 1.0);
     }
-    const offscreenCanvas = document.createElement("canvas");
-    offscreenCanvas.width = w;
-    offscreenCanvas.height = h;
-    return offscreenCanvas;
-  }
+  `;
+const vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
 
-  getOffscreenGradientData() {
-    const w = this.gradientWidth;
-    const h = this.gradientHeight;
-    this.offscreenCtx.fillStyle = this.offscreenGradient;
-    this.offscreenCtx.fillRect(0, 0, w, h);
-    const imageData = this.offscreenCtx.getImageData(0, 0, w, h);
-    return imageData.data;
-  }
+// Fragment shader
+const fragCode = `
+    // fragment shaders don't have a default precision so we need
+    // to pick one. mediump is a good default
+    precision mediump float;
+  
+    uniform float u_width;
+    uniform float u_height;
+    uniform vec4 u_color1;
+    uniform vec4 u_color2;
+  
+    void main(void) {
+      vec2 st = gl_FragCoord.xy;
+      float half_width = u_width / 2.0;
+      float half_height = u_height / 2.0;
 
-  colourForProportion(gradientData, proportion) {
-    const x = Math.floor((gradientData.length / 4) * proportion) * 4;
-    return gradientData.slice(x, x + 4);
-  }
-
-  draw() {
-    const gradientData = this.getOffscreenGradientData();
-    const ctx = this.ctx;
-    const centreX = this.x0 + this.width / 2;
-    const centreY = this.y0 + this.height / 2;
-    const rectSize = 1;
-    for (let y = this.y0; y < this.y1; y += rectSize) {
-      let dY = y - centreY;
-      for (let x = this.x0; x < this.x1; x += rectSize) {
-        let dX = x - centreX;
-        let angle = (Math.atan(dX / dY) * 180) / Math.PI;
-        if (dX < 0) {
-          angle = 360 - angle;
-          if (dY >= 0) {
-            angle = angle - 180;
-          }
-        } else {
-          angle = angle * -1;
-          if (dY >= 0) {
-            angle = angle + 180;
-          }
-        }
-        let rgba = this.colourForProportion(gradientData, angle / 360);
-        let [r, g, b, a] = rgba;
-        ctx.fillStyle = `rgba(${r},${g},${b},${a / 255})`;
-        ctx.fillRect(x, y, rectSize, rectSize);
-      }
+      float percent = (atan(st[0] - half_width, half_height - st[1]) - 3.14) / -0.785;
+      gl_FragColor = mix(u_color1, u_color2, percent);
     }
-  }
+  `;
+const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+// Create a shader program object to store
+// the combined shader program
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader);
+gl.attachShader(shaderProgram, fragShader);
+gl.linkProgram(shaderProgram);
+gl.useProgram(shaderProgram);
+
+/*======= Associating shaders to buffer objects =======*/
+
+// Bind vertex buffer object
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
+// Colors. vec4 [r, g, b, a]
+const color1Loc = gl.getUniformLocation(shaderProgram, "u_color1");
+gl.uniform4fv(color1Loc, [0, 0, 0, 1]);
+const color2Loc = gl.getUniformLocation(shaderProgram, "u_color2");
+gl.uniform4fv(color2Loc, [0, 1, 0, 1]);
+
+// Width & height
+const heightLoc = gl.getUniformLocation(shaderProgram, "u_height");
+gl.uniform1f(heightLoc, height);
+const widthLoc = gl.getUniformLocation(shaderProgram, "u_width");
+gl.uniform1f(widthLoc, width);
+
+// Get the attribute location
+const coord = gl.getAttribLocation(shaderProgram, "coordinates");
+gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(coord);
+gl.enable(gl.DEPTH_TEST);
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+// Draw the square
+gl.drawElements(gl.TRIANGLE_FAN, indices.length, gl.UNSIGNED_SHORT, 0);
+
+var output = document.getElementById("output");
+output.height = height;
+output.width = width;
+
+var amplitud = 180;
+
+var grado_posicion = cd;
+var grado_posicion_rad = grado_posicion * ( Math.PI / 180 );
+var posicion_x = vl.x_circum + vl.radius_circum * Math.sin(grado_posicion_rad);
+var posicion_y = vl.y_circum - vl.radius_circum * Math.cos(grado_posicion_rad);
+
+var grado_arco_ini = grado_posicion + 270;
+var grado_arco_rad_ini = grado_arco_ini * ( Math.PI / 180 );
+
+var grado_arco_fin = grado_arco_ini + amplitud;
+var grado_arco_rad_fin = grado_arco_fin * ( Math.PI / 180 );
+
+
+const ctx = output.getContext("2d");
+//console.log(vl.w_square)
+ctx.beginPath();
+ctx.rect(0, 0, vl.w_square, vl.h_square);
+ctx.stroke();
+
+const pattern = ctx.createPattern(canvas, "no-repeat");
+ctx.beginPath();
+
+ctx.fillStyle = pattern;
+
+ctx.moveTo(vl.x_circum, vl.y_circum);
+
+
+ctx.lineTo(posicion_x,posicion_y);
+ctx.arc(vl.x_circum, vl.y_circum,vl.radius_circum, grado_arco_rad_ini, grado_arco_rad_fin);
+//ctx.closePath();
+ctx.lineTo(vl.x_circum, vl.y_circum);
+ctx.fill();
+//ctx.stroke();
+
+
 }
